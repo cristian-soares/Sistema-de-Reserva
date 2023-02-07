6b647fb7562dda5d258da28139bf23d89b92b360
Carolmsouza29 <100072869+Carolmsouza29@users.noreply.github.com>
2023-02-07 19:29:00 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,33 +1,39 @@
+    //Classe Carro, representa um dos veículos disponíveis para aluguel
     public class Carro extends Veiculo{
         private boolean ehAutomatico;  
-        private double valorEntrada; //Valor pago antes da reserva, utilizado para calcular o Aluguel
-
+        private double valorEntrada; 
+        
+    //Construtor da classe 
     public Carro(String marca, String modelo, boolean disponivel, int anoFabricacao,double kmRodados, boolean ehAutomatico, double valorEntrada){
         super(marca, modelo, disponivel, anoFabricacao, kmRodados);
         this.ehAutomatico = ehAutomatico;
         this.valorEntrada = valorEntrada;
     }
-
+    
+    //Metodo para retornar o estado do atributo ehAutomático
     public boolean getEhAutomatico(){
         return ehAutomatico;
     }
-
+    
+    //Metodo para retornar o estado do atributo valorEntrada
     public double getValorEntrada(){
         return valorEntrada;
     }
 
+    //Metodo que descreve as caracteristicas do carro
     @Override
     public String toString(){
         return super.toString() + "\nCategoria: " + getEhAutomatico() + "\nValor de Entrada: " + getValorEntrada();
     }
     
+    //Metodo para calcular o aluguel
     @Override
     public double calcularAluguel() {
         if (!ehAutomatico) {
-            return getAnoFabricacao() - (getKmRodados() * 0.015);
+            return valorEntrada+(getAnoFabricacao() - (getKmRodados() * 0.015));
         }
         else {
-        return getAnoFabricacao() - (getKmRodados() * 0.01);
+        return valorEntrada+(getAnoFabricacao() - (getKmRodados() * 0.01));
         }
     }
     
