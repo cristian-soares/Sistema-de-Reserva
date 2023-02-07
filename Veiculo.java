8583551e4a1c2d2a58b8636c397ae605c23f35f1
Carolmsouza29 <100072869+Carolmsouza29@users.noreply.github.com>
2023-02-07 19:51:26 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,11 +1,12 @@
+    //Classe que possui os atributos e metodos comuns de todos os veiculos disponiveis para aluguel
     public abstract class Veiculo {
         private String marca;
         private String modelo;
         private boolean disponivel;
         private int anoFabricacao;
         private double kmRodados;
-        //verificar se está disponível
-
+  
+    //Construtor da classe
     public Veiculo(String marca, String modelo, boolean disponivel, int anoFabricacao, double kmRodados){
         this.marca = marca;
         this.modelo = modelo;
@@ -14,31 +15,32 @@
         this.kmRodados = kmRodados;
     }
 
-    
+    //Metodo que retorna qual a Marca do veiculo
     public String getMarca(){
         return marca;
     }
-
+    
+    //Metodo que retorna qual o Modelo
     public String getModelo(){
         return modelo;
     }
-
+    //Metodo que retorna se o veiculo está disponivel ou não
     public boolean getDisponivel(){
         return disponivel;
     }
-
+    //Metodo que retorna qual o ano de fabricacao
     public int getAnoFabricacao(){
         return anoFabricacao;
     }
-    
+    //Metodo que retorna a quantidade de Km rodados
     public double getKmRodados() {
         return kmRodados;
     }
-    
+    //Metodo que retorna a descricao do veiculo
     @Override
     public String toString(){
         return ("Marca: " + getMarca() + "\nModelo: " + getModelo() + "\nQuantidade Disponivel: " + getDisponivel() + "\nAno de fabricação: " + getAnoFabricacao());
     }
-    
+    //Metodo referente ao calculo do valor do auguel, sem implementacao que sera sobrescrito nas subclasses 
     public abstract double calcularAluguel();
 }
