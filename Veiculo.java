74191d049dfed2391d4ad9983f51abea5e547c5c
Carolmsouza29 <100072869+Carolmsouza29@users.noreply.github.com>
2023-02-11 09:53:34 -0300

    Update Veiculo.java

--- Veiculo.java
+++ Veiculo.java
@@ -1,18 +1,20 @@
-    //Classe que possui os atributos e metodos comuns de todos os veiculos disponiveis para aluguel
+        //Classe que possui os atributos e metodos comuns de todos os veiculos disponiveis para aluguel
     public abstract class Veiculo {
         private String marca;
         private String modelo;
-        private boolean disponivel;
+        private String disponivel;
         private int anoFabricacao;
         private double kmRodados;
+        private int id;
   
     //Construtor da classe
-    public Veiculo(String marca, String modelo, boolean disponivel, int anoFabricacao, double kmRodados){
+    public Veiculo(String marca, String modelo, String disponivel, int anoFabricacao, double kmRodados, int id){
         this.marca = marca;
         this.modelo = modelo;
         this.disponivel = disponivel;
         this.anoFabricacao = anoFabricacao;
         this.kmRodados = kmRodados;
+        this.id = id;
     }
 
     //Metodo que retorna qual a Marca do veiculo
@@ -25,7 +27,7 @@
         return modelo;
     }
     //Metodo que retorna se o veiculo está disponivel ou não
-    public boolean getDisponivel(){
+    public String getDisponivel(){
         return disponivel;
     }
     //Metodo que retorna qual o ano de fabricacao
@@ -36,11 +38,15 @@
     public double getKmRodados() {
         return kmRodados;
     }
+    //Metodo que retorna o id
+    public double getId() {
+        return id;
+    }
     //Metodo que retorna a descricao do veiculo
     @Override
     public String toString(){
         return ("Marca: " + getMarca() + "\nModelo: " + getModelo() + "\nQuantidade Disponivel: " + getDisponivel() + "\nAno de fabricação: " + getAnoFabricacao());
     }
-    //Metodo referente ao calculo do valor do auguel, sem implementacao que sera sobrescrito nas subclasses 
+    //Metodo referente ao calculo do valor do aluguel, sem implementacao que sera sobrescrito nas subclasses 
     public abstract double calcularAluguel();
 }
