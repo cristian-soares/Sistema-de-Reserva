6f5a8f8d6736828365af864a5ad6ce820f8bf93b
Carolmsouza29 <100072869+Carolmsouza29@users.noreply.github.com>
2023-02-11 09:54:20 -0300

    Update Reserva.java

--- Reserva.java
+++ Reserva.java
@@ -1,20 +1,23 @@
+import java.util.*;
 
 public class Reserva {
     private String nomeCliente;
     private String cpfCliente;
-    private Veiculo veiculos;
+    private List<Veiculo> reservaVeiculos;
     private String tempoReserva;
 
     public Reserva(String nomeCliente, String cpf ,Veiculo veiculo, String tempoReserva){
         this.nomeCliente = nomeCliente;
-        cpfCliente = cpf;
+        this.cpfCliente = cpf;
         this.veiculo = veiculo;
         this.tempoReserva = tempoReserva;
+        reservaVeiculos = new ArrayList<>();
   
     }
     
-    public void addVeiculo(veic){
-        veiculos.add(veic);
+    public void realizarReserva(veic){
+        //pegar o metodo qu retorna se o veiculo esta disponivel ou não
+        reservaVeiculos.add(veic);
     }
 
     public String getNomeCliente(){
@@ -31,5 +34,16 @@ public class Reserva {
 
     public String getTempoReserva(){
         return tempoReserva;
+    }
+    public void cancelarReserva(String cpf){
+        for(Veiculo rv: reservaVeiculos){
+            if(getCpfCliente().equals(cpf)){
+                reservaVeiculos.remove(rv);
+            }
+            else{
+                System.out.println("O CPF solicitado não realizou nenhuma reserva");
+            }
+        }
+
     }
 }
