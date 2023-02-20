e12e6563d6e983f150898022ec3551bbeb0e99c8
cristian-soares <cmatheussoares@gmail.com>
2023-02-20 20:51:31 -0300

    Adição de tempo

--- Reserva.java
+++ Reserva.java
@@ -2,12 +2,15 @@ public class Reserva {
     private String nomeCliente;
     private String cpfCliente;
     private Veiculo veiculo;
+    private String dataInicio;
     private int tempoReserva;
 
-    public Reserva(Veiculo veiculo, String nomeCliente, String cpf , int tempoReserva){
+
+    public Reserva(Veiculo veiculo, String nomeCliente, String cpf , String dataInicio ,int tempoReserva){
         this.nomeCliente = nomeCliente;
         this.cpfCliente = cpf;
         this.veiculo = veiculo;
+        this.dataInicio = dataInicio;
         this.tempoReserva = tempoReserva;
     }
 
@@ -23,13 +26,17 @@ public class Reserva {
         return veiculo;
     }
 
+    public String getDataInicio(){
+        return dataInicio;
+    }
+
     public int getTempoReserva(){
         return tempoReserva;
     }
 
     @Override 
     public String toString(){
-        return ("Nome cliente" + getNomeCliente() + "/nCPF: " + getCpfCliente() + "/nVeiculo alugado: " + getVeiculo() + "/nTempo Reserva: " +getTempoReserva());
+        return ("Nome cliente" + getNomeCliente() + "/nCPF: " + getCpfCliente() + "/nVeiculo alugado: " + getVeiculo() +"/nData de Inicio da Reserva: "+ getDataInicio() + "/nTempo Reserva: " +getTempoReserva());
     }
   
 }
